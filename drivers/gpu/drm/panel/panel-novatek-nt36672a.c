2d8a38f072c74315808254d70920c338254c6ef9
Alexey Minnekhanov <alexeymin@postmarketos.org>
2025-06-20 19:08:38 +0300

    drm/panel: nt36672a: Switch to modern refcounted allocation API
    Use the new refcounted allocation API devm_drm_panel_alloc() to allocate
    the panel in place of devm_kzalloc().
    
    Remove call to drm_panel_init() because it's part of devm_drm_panel_alloc().
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@postmarketos.org>

--- drivers/gpu/drm/panel/panel-novatek-nt36672a.c
+++ drivers/gpu/drm/panel/panel-novatek-nt36672a.c
@@ -616,7 +616,6 @@ static int nt36672a_panel_add(struct nt36672a_panel *pinfo)
 		return dev_err_probe(dev, PTR_ERR(pinfo->reset_gpio),
 				     "failed to get reset gpio from DT\n");
 
-	drm_panel_init(&pinfo->base, dev, &panel_funcs, DRM_MODE_CONNECTOR_DSI);
 	pinfo->base.prepare_prev_first = true;
 
 	ret = drm_panel_of_backlight(&pinfo->base);
@@ -634,9 +633,10 @@ static int nt36672a_panel_probe(struct mipi_dsi_device *dsi)
 	const struct nt36672a_panel_desc *desc;
 	int err;
 
-	pinfo = devm_kzalloc(&dsi->dev, sizeof(*pinfo), GFP_KERNEL);
-	if (!pinfo)
-		return -ENOMEM;
+	pinfo = devm_drm_panel_alloc(&dsi->dev, struct nt36672a_panel, base,
+				     &panel_funcs, DRM_MODE_CONNECTOR_DSI);
+	if (IS_ERR(pinfo))
+		return PTR_ERR(pinfo);
 
 	desc = of_device_get_match_data(&dsi->dev);
 	dsi->mode_flags = desc->mode_flags;
